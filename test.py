a289abc1a0dcffc906febfba0eb484c1603b8983
Andrés Martínez González <andres.geotec@gmail.com>
2020-11-24 10:01:13 -0600

    rampa de colores invertida

--- test.py
+++ test.py
@@ -1,33 +1,139 @@
+from qgis.PyQt.QtXml import QDomDocument
+from qgis.utils import iface
+import os
+
+#methodName = 'Jenks'
+methodName = 'EqualInterval'
+
+dataMap = {
+    'file': "ingreso_sintomas_todos",
+    'title': 'Accesibilidad hospitalaria',
+    'subtitle': 'Tiempo promedio entre fecha de síntomas e ingreso',
+    'date': '22 de Noviembre de 2020',
+    'note': "Promedio municipal de días desde 'FECHA_SINTOMAS' a 'FECHA_INGRESO'. Para los municipios con menos de 3 casos positivos se utilizó el promedio por jurisdicción sanitaria.",
+    'source': 'Secretaría de Salud: "201119COVID19MEXICOTOT" de la Dirección General de Epidemiología',
+    'legend': 'Días Promedio',
+    'variant': 'tiempo_ingreso_sintomas',
+    'targetFieldName': 'cvegeomun',
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'RdYlGn'
+}
+
+#Instance path proyect application
 path = '/home/andres/Proyectos/CONACyT/AtencionHospitalaria/auto-map-tiempo-hospital/data'
 os.chdir(path)
-
 project = QgsProject()
-layout = QgsPrintLayout(project)
-layout.initializeDefaults()
+
+symbol_edos = QgsFillSymbol.createSimple({
+    'outline_width': '0.86',
+    'outline_color': '35,35,35,255',
+    'offset_unit': 'MM',
+    'color': '0,0,0,255',
+    'outline_style': 'solid',
+    'style': 'no',
+    'joinstyle': 'bevel',
+    'outline_width_unit': 'MM'
+})
+
+
+def loadLayerGpkg(file, layername, name, symbol=None):
+    gpkg_layer = file + "|layername=" + layername
+    vectorlayer = QgsVectorLayer(gpkg_layer, name, "ogr")
+    if symbol:
+        vectorlayer.renderer().setSymbol(symbol)
+    return vectorlayer
+
+def loadCsvFile(file):
+    uri = f"file://{os.getcwd()}/{file}.csv?type=csv&delimiter=,&detectTypes=no"
+    csv = QgsVectorLayer(uri, file, 'delimitedtext')
+    #QgsProject.instance().addMapLayer(muns)
+    #QgsProject.instance().addMapLayer(csv)
+    return csv
+
+def createJoinData(csv, targetFieldName):
+    join = QgsVectorLayerJoinInfo()
+    join.setJoinFieldName(targetFieldName)
+    join.setTargetFieldName(targetFieldName)
+    join.setJoinLayer(csv)
+    join.setUsingMemoryCache(True)
+    #join.setPrefix('_')
+    return join
+
+def loadTempleteQpt(file):
+    #Read qpt file
+    templateFile = open(file, 'rt')
+    templateContent = templateFile.read()
+    templateFile.close()
+
+    #Create document
+    document = QDomDocument()
+    document.setContent(templateContent)
+    return document
+
+def graduatedMethod(name):
+    if (name == 'Jenks'):
+        return QgsGraduatedSymbolRenderer.Jenks
+    if (name == 'EqualInterval'):
+        return QgsGraduatedSymbolRenderer.EqualInterval
+
+def invertColorRamp(colorRamp):
+    invert = QgsGradientColorRamp()
+    invert.setColor1(colorRamp.color2())
+    invert.setColor2(colorRamp.color1())
+    stops = colorRamp.stops().copy()
+    newStops = []
+    for i in range(len(stops)):
+        #print(i, len(stops)-i-1)
+        newStops.append(colorRamp.stops()[i])
+        newStops[i].color = stops[len(stops)-i-1].color
+        #print(stop.offset, stop.color.name())
+    invert.setStops(newStops)
+    return invert
+    
+def addClasification(targetFieldNameData, methodName, colorRampName, legendName):
+    print(targetFieldNameData)
+    muns.setRenderer(QgsGraduatedSymbolRenderer(targetFieldNameData))
+    muns.renderer().updateClasses(muns, graduatedMethod(methodName), 5)
+    ramp = QgsStyle().defaultStyle().colorRamp(colorRampName)
+    muns.renderer().updateColorRamp(invertColorRamp(ramp))
+    muns.renderer().updateSymbols(QgsFillSymbol.createSimple({'outline_width': '0.05'}))
+    muns.setName(dataMap['legend'])
+    text = muns.renderer().legendSymbolItems()[0].label
+    muns.renderer().legendSymbolItems()[0].label = f'{text} ()'
+    muns.triggerRepaint()
+    #iface.layerTreeView().refreshLayerSymbology(muns.id())
+
+def createLayout(templeteQpt):
+    #Create document
+    document = loadTempleteQpt(templeteQpt)
+    layout = QgsPrintLayout(project)
+    layout.initializeDefaults()
+    layout.loadFromTemplate(document, QgsReadWriteContext())
+    return layout
+
+
 
 #Municipios
-path_muns_gpkg = "mun_2019.gpkg"
-gpkg_muns_layer = path_muns_gpkg + "|layername=mun_2019"
-muns = QgsVectorLayer(gpkg_muns_layer, "Municipios", "ogr")
+muns = loadLayerGpkg('mun_2019.gpkg', 'mun_2019', 'Municipios')
 
-map = QgsLayoutItemMap(layout)
-map.setLayers([muns])
-map.setRect(QRectF(0, 0, 200, 200))
-map.setFrameEnabled(True)
-layout.addItem(map)
-
-legend = QgsLayoutItemLegend(layout)
-legend.setRect(QRectF(0, 0, 30, 30))
-legend.setFrameEnabled(True)
-legend.setLinkedMap(map) # map is an instance of QgsLayoutItemMap
-layout.addItem(legend)
+#Estados
+edos = loadLayerGpkg('edos_2019.gpkg', 'edos_2019', 'Estados', symbol_edos)
 
 
+#def createMap(dataMap):
+#for dataMap in dataMaps[:2]:
+print('Creando mapa', dataMap['file'])
 
-#Instance Iamage Path
-base_path = os.path.join(QgsProject.instance().homePath())
-image_path = os.path.join(base_path, "test.png")
+QgsProject.instance().addMapLayer(muns)
 
-#Export Image
-exporter = QgsLayoutExporter(layout)
-exporter.exportToImage(image_path, QgsLayoutExporter.ImageExportSettings())
+#Prepare data in layer
+csv = loadCsvFile(dataMap['file'])
+join = createJoinData(csv, dataMap['targetFieldName'])
+muns.addJoin(join)
+
+#Add Clasification
+targetFieldNameData = f"{dataMap['file']}_{dataMap['variant']}"
+addClasification(targetFieldNameData, methodName, dataMap['colorRamp'], dataMap['legend'])
+
+iface.layerTreeView().refreshLayerSymbology(muns.id())
+
--- tiemposHospitalarios.py
+++ tiemposHospitalarios.py
@@ -2,17 +2,21 @@ from qgis.PyQt.QtXml import QDomDocument
 from qgis.utils import iface
 import os
 
+#methodName = 'Jenks'
+methodName = 'EqualInterval'
+
 dataMaps = [{
     'file': "ingreso_sintomas_todos",
     'title': 'Accesibilidad hospitalaria',
-    'subtitle': 'Tiempo promedio entre fecha de síntomas  e ingreso',
+    'subtitle': 'Tiempo promedio entre fecha de síntomas e ingreso',
     'date': '22 de Noviembre de 2020',
     'note': "Promedio municipal de días desde 'FECHA_SINTOMAS' a 'FECHA_INGRESO'. Para los municipios con menos de 3 casos positivos se utilizó el promedio por jurisdicción sanitaria.",
     'source': 'Secretaría de Salud: "201119COVID19MEXICOTOT" de la Dirección General de Epidemiología',
     'legend': 'Días Promedio',
     'variant': 'tiempo_ingreso_sintomas',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'RdYlGn'
 }, {
     'file': 'resultado_ingreso_todes',
     'title': 'Respuesta hospitalaria',
@@ -23,7 +27,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_resultado_ingreso',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'RdYlGn'
 }, {
     'file': 'alta_ingreso_hospitalizados',
     'title': 'Respuesta hospitalaria',
@@ -34,7 +39,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_alta_ingreso',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'Greens'
 }, {
     'file': 'defuncion_ingreso_fallecidos',
     'title': 'Respuesta hospitalaria',
@@ -45,7 +51,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_defuncion_ingreso',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'Greys'
 }, {
     'file': 'alta_resultado_hospitalizados',
     'title': 'Respuesta hospitalaria',
@@ -56,7 +63,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_alta_resultado',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'Greens'
 }, {
     'file': 'defuncion_resultado_fallecidos',
     'title': 'Respuesta hospitalaria',
@@ -67,7 +75,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_defuncion_resultado',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'Greys'
 }, {
     'file': 'resultado_ingreso_ambulatorios',
     'title': 'Respuesta hospitalaria',
@@ -78,7 +87,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_resultado_ingreso',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'RdYlGn'
 }, {
     'file': 'resultado_ingreso_hospitalizados',
     'title': 'Respuesta hospitalaria',
@@ -89,7 +99,8 @@ dataMaps = [{
     'legend': 'Días Promedio',
     'variant': 'tiempo_resultado_ingreso',
     'targetFieldName': 'cvegeomun',
-    'template': 'respuestaHospitalaria.qpt'
+    'template': 'respuestaHospitalaria.qpt',
+    'colorRamp': 'RdYlGn'
 }]
 
 
@@ -120,7 +131,6 @@ def loadLayerGpkg(file, layername, name, symbol=None):
 def loadCsvFile(file):
     uri = f"file://{os.getcwd()}/{file}.csv?type=csv&delimiter=,&detectTypes=no"
     csv = QgsVectorLayer(uri, file, 'delimitedtext')
-    #csv.isValid() s.replace('a', '')
     #QgsProject.instance().addMapLayer(muns)
     #QgsProject.instance().addMapLayer(csv)
     return csv
@@ -145,13 +155,59 @@ def loadTempleteQpt(file):
     document.setContent(templateContent)
     return document
 
+def graduatedMethod(name):
+    if (name == 'Jenks'):
+        return QgsGraduatedSymbolRenderer.Jenks
+    if (name == 'EqualInterval'):
+        return QgsGraduatedSymbolRenderer.EqualInterval
 
-#for dataMap in dataMaps:
-def createMap(dataMap):
+def invertColorRamp(colorRamp):
+    invert = QgsGradientColorRamp()
+    invert.setColor1(colorRamp.color2())
+    invert.setColor2(colorRamp.color1())
+    stops = colorRamp.stops().copy()
+    newStops = []
+    for i in range(len(stops)):
+        #print(i, len(stops)-i-1)
+        newStops.append(colorRamp.stops()[i])
+        newStops[i].color = stops[len(stops)-i-1].color
+        #print(stop.offset, stop.color.name())
+    invert.setStops(newStops)
+    return invert
+
+def addClasification(targetFieldNameData, methodName, colorRamp, legendName):
+    print(targetFieldNameData)
+    muns.setRenderer(QgsGraduatedSymbolRenderer(targetFieldNameData))
+    muns.renderer().updateClasses(muns, graduatedMethod(methodName), 5)
+    ramp = QgsStyle().defaultStyle().colorRamp(colorRamp)
+    muns.renderer().updateColorRamp(invertColorRamp(ramp))
+    muns.renderer().updateSymbols(QgsFillSymbol.createSimple({'outline_width': '0.05'}))
+    muns.setName(dataMap['legend'])
+    text = muns.renderer().legendSymbolItems()[0].label
+    muns.renderer().legendSymbolItems()[0].label = f'{text} ()'
+    muns.triggerRepaint()
+
+def createLayout(templeteQpt):
+    #Create document
+    document = loadTempleteQpt(templeteQpt)
+    layout = QgsPrintLayout(project)
+    layout.initializeDefaults()
+    layout.loadFromTemplate(document, QgsReadWriteContext())
+    return layout
+
+
+
+#Municipios
+muns = loadLayerGpkg('mun_2019.gpkg', 'mun_2019', 'Municipios')
+
+#Estados
+edos = loadLayerGpkg('edos_2019.gpkg', 'edos_2019', 'Estados', symbol_edos)
+
+
+#def createMap(dataMap):
+#for dataMap in dataMaps[:2]:
+for dataMap in dataMaps[:1]:
     print('Creando mapa', dataMap['file'])
-
-    #Municipios
-    muns = loadLayerGpkg('mun_2019.gpkg', 'mun_2019', 'Municipios')
 
     #Prepare data in layer
     csv = loadCsvFile(dataMap['file'])
@@ -160,25 +216,10 @@ def createMap(dataMap):
 
     #Add Clasification
     targetFieldNameData = f"{dataMap['file']}_{dataMap['variant']}"
-    print(targetFieldNameData)
-    muns.setRenderer(QgsGraduatedSymbolRenderer(targetFieldNameData))
-    #muns.renderer().updateClasses(muns, QgsGraduatedSymbolRenderer.Jenks, 5)
-    muns.renderer().updateClasses(muns, QgsGraduatedSymbolRenderer.EqualInterval, 5)
-    ramp = QgsStyle().defaultStyle().colorRamp('RdYlGn')
-    muns.renderer().updateColorRamp(QgsGradientColorRamp(ramp))
-    muns.renderer().updateSymbols(QgsFillSymbol.createSimple({'outline_width': '0.05'}))
-    muns.setName(dataMap['legend'])
-
-    #Estados
-    edos = loadLayerGpkg('edos_2019.gpkg', 'edos_2019', 'Estados', symbol_edos)
-
-    #Create document
-    document = loadTempleteQpt(dataMap['template'])
+    addClasification(targetFieldNameData, methodName, dataMap['colorRamp'], dataMap['legend'])
 
     #Create layout
-    layout = QgsPrintLayout(project)
-    layout.initializeDefaults()
-    layout.loadFromTemplate(document, QgsReadWriteContext())
+    layout = createLayout(dataMap['template'])
 
     ##Add Texts
     layout.itemById('title').setText(dataMap['title'])
@@ -204,13 +245,15 @@ def createMap(dataMap):
 
     #Instance Iamage Path
     base_path = os.path.join(QgsProject.instance().homePath())
-    image_path = os.path.join(base_path, f"{dataMap['file']}.png")
+    image_name = f"{dataMap['file']}_{methodName}.png"
+    image_path = os.path.join(base_path, image_name)
 
     #Export Image
     exporter = QgsLayoutExporter(layout)
     exporter.exportToImage(image_path, QgsLayoutExporter.ImageExportSettings())
 
     muns.removeJoin(join.joinLayerId())
-    print('Mapa generado', dataMap['file'])
+    print('Mapa generado', image_name)
     
-createMap(dataMaps[0])
+#createMap(dataMaps[0])
+print('*** Proceso finalizado ***')
